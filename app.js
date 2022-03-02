e506876439a9522956b357067e39b7f95e6e6ba5
wqpqptd <bmdi1013@gmail.com>
2022-03-03 00:57:57 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError, errorHandler } = require("./app/errors");
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to contac book application."});
+app.get("/",(req, res) => {
+    res.json({ message: "Welcome to contact book application."});
 });
 
-setupContactRoutes(app); 
+setupContactRoutes(app);
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    errorHandler.handleError(err, res);
+});
 
 module.exports = app;
