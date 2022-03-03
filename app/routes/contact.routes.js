18c36aa349b0d35d9c823a840843a9bb1639b3eb
wqpqptd <bmdi1013@gmail.com>
2022-03-03 22:01:53 +0700

    Cai dat xu ly loi

--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -1,34 +1,5 @@
-// const express = require("express");
-// const contacts = require("../controllers/contact.controller");
-
-const express = require("express")
-
-// module.exports = (app) => {
-//     const router = express.Router();
-
-//     //Retrieve all contacts
-//     router.get("/", contacts.findAll);
-
-//     //Create a new contact
-//     router.post("/", contacts.create); 
-
-//     // Delete all contacts
-//     router.delete("/", contacts.deleteAll);
-
-//     // Retrieve all favorite contacts
-//     router.get("/favorite", contacts.FindAllFavorite);
-
-//     // Retrieve a single contact with id
-//     router.get("/:id", contacts.findOne);
-
-//     //Update a contact with id
-//     router.put("/:id", contacts.update);
-
-//     //Delete a contact with id
-//     router.delete("/:id", contacts.delete);
-
-//     app.use("/api/contacts", router);
-// };
+const express = require("express");
+const contacts = require("../controllers/contact.controller");
 
 module.exports = (app) => {
     const router = express.Router();
